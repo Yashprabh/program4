9e8eed41e0e253eeef157380c6eb40dfc613e8a9
Yashprabh <158807048+Yashprabh@users.noreply.github.com>
2025-07-17 23:00:40 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.program4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnExplicitContent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnExplicitContent=findViewById(R.id.btnExplicitContent);
+        btnExplicitContent.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this,
+                    SecondActivity.class);
+            startActivity(intent);
+        }
+        });
+    }
+}
