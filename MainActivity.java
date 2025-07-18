d63a7b5945a37eb63ccf3e2b156118e8dea5c245
Yashprabh <158807048+Yashprabh@users.noreply.github.com>
2025-07-18 14:25:22 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.program4;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -12,20 +11,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnExplicitContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        btnExplicitContent=findViewById(R.id.btnExplicitContent);
-        btnExplicitContent.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(MainActivity.this,
-                    SecondActivity.class);
-            startActivity(intent);
-        }
-        });
+    }
+
+    public void onImplicitButtonClicked(View view) {
+        Uri url = Uri.parse("https://www.google.com");
+        Intent i = new Intent(Intent.ACTION_VIEW, url);
+        startActivity(i);
+    }
+
+    public void onExplicitButtonClicked(View view) {
+        Intent i = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(i);
     }
 }
