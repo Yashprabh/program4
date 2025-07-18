7cb5b51cf817c37cd462ce398d53bb9d4406617b
Yashprabh <158807048+Yashprabh@users.noreply.github.com>
2025-07-18 14:24:27 +0530

    Update SecondActivity.java

--- SecondActivity.java
+++ SecondActivity.java
@@ -1,11 +1,6 @@
 package com.example.program4;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,19 +10,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
 
-    Button btnImplicitContent;
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second);
-        btnImplicitContent=findViewById(R.id.btnImplicitContent);
-        btnImplicitContent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri webpage = Uri.parse("https://www.google.com");
-                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-                startActivity(intent);
-            }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
-    }}
+    }
+}
